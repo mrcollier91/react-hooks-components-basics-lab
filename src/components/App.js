86babe5747c7aa5d354ce24eb680b58c1315eb1c
mrcollier91 <colliermatt91@gmail.com>
2022-07-26 10:04:03 -0500

    commit tree fd166cc8fc4aac2fe842224e25c863ff7748f7fb

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 function NavBar() {
   return (
     <nav>
@@ -7,6 +8,8 @@ function NavBar() {
     </nav>
   );
 }
+
+
 
 function Home() {
   return (
@@ -16,15 +19,22 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
